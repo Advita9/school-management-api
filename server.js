5997d0e88e27d2be802f68bb1dbe55b4e9207f0b
Advita Shrivastava <advitashrivastava09@gmail.com>
2024-08-26 13:08:01 +0530

    Added alt port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mysql = require("mysql2");
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
